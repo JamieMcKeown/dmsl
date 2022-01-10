3fca511770903e55ae5b1aef4d88ba76794f7268
Jamie McKeown <52748398+JamieMcKeown@users.noreply.github.com>
2022-01-09 22:21:21 -0500

    WIP - backend migrations
    Users migration created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,25 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('id');
+
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+
+            $table->char('contact_preference', 50)->nullable();
+
+            $table->enum('available_days', array('Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'))->nullable();
+            $table->enum('available_times', array(7, 9, 'both'))->nullable();
+            $table->enum('available_division', array('Shields', 'MacDonald', 'either'))->nullable();
+            $table->enum('available_position', array('1', '2', '3', 'SS', 'Catcher', 'Pitcher', 'Rover', 'Outfield', 'any'))->nullable();
+
+            $table->foreign('team_id')->references('id')->on('teams');
+
+            $table->boolean('is_online');
+ 
             $table->rememberToken();
             $table->timestamps();
         });
