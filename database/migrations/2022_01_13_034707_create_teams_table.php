64c3aa57b45e17670634d5ed8ca23e03cebc5963
Jamie McKeown <52748398+JamieMcKeown@users.noreply.github.com>
2022-01-12 23:17:47 -0500

    WIP -  created Team

---
+++ database/migrations/2022_01_13_034707_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('division');
+            $table->string('name');
+            $table->string('coach_first_name')->nullable();
+            $table->string('coach_last_name')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
